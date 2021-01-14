d7e1095271c431a0ee40db13ef58ef565a880e51
habbababbai <50295184+habbababbai@users.noreply.github.com>
2021-01-14 16:56:17 +0100

    Zadanie FMOD

---
+++ Assets/_Code/AmbienceAudioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class AmbienceAudioController : MonoBehaviour
+{
+    [SerializeField] private GameObject outsideAudio;
+    [SerializeField] private GameObject insideAudo;
+    private bool isPlayerInside = false;
+
+    private void Start()
+    {
+        outsideAudio.SetActive(true);
+        insideAudo.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInside = !isPlayerInside;
+            Debug.Log($"Player inside: {isPlayerInside}");
+            if (isPlayerInside)
+            {
+                insideAudo.SetActive(true);
+                outsideAudio.SetActive(false);
+            }
+            else
+            {
+                insideAudo.SetActive(false);
+                outsideAudio.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/_Code/Door.cs
@@ -0,0 +1,27 @@
+﻿using FMODUnity;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Door : MonoBehaviour {
+    [SerializeField] Animator animator;
+    [SerializeField] UnityEvent onDoorOpen = new UnityEvent();
+    [SerializeField] UnityEvent onDoorClose = new UnityEvent();
+    [SerializeField, EventRef] private string doorEvent;
+    
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetBool("character_nearby", true);
+            onDoorOpen.Invoke();
+            RuntimeManager.PlayOneShot(doorEvent, transform.position);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetBool("character_nearby", false);
+            onDoorClose.Invoke();
+            RuntimeManager.PlayOneShot(doorEvent, transform.position);
+        }
+    }
+}
